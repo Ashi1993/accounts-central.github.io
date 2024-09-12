9fa8a245d1a510fe444f72efe6365285387def34
Ashirwada <ashirwada93@gmail.com>
2024-09-12 14:03:54 +0530

    fix issue

--- App/assets/js/AccountAggregator.js
+++ App/assets/js/AccountAggregator.js
@@ -135,7 +135,7 @@ function doAccInitiation() {
     };
     console.log("doAccInitiation requestOptions ", requestOptions);
 
-    fetch("https://localhost:8243/xs2a/v1/consents", requestOptions)
+    fetch("http://localhost:8280/xs2a/v1/consents", requestOptions)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
