c5355ce69cbbf4d741b716f3479d24427c590dc2
Ashirwada <ashirwada93@gmail.com>
2024-09-18 21:24:27 +0530

    js update

--- App/assets/js/modal.js
+++ App/assets/js/modal.js
@@ -1,5 +1,46 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
     $('#modal1').modal('open');
+});
+
+$(function () {
+    console.log("demos.js loaded");
+    $("a:not([role=main-action])").click(function (e) {
+        e.preventDefault();
+    });
+
+    if ($('html').hasClass('bank')) {
+        var citizenBank = {
+            title: "Maurishes Commencial Bank Open Banking Solution",
+            footer: "Maurishes Commencial Bank",
+            style: "css/citizenbank.css",
+            logo: "img/mcb.jpg"
+        },
+            eudcBank = {
+                title: "EUDC Bank Open Banking Solution",
+                footer: "EUDC Bank",
+                style: "css/eudcbank.css",
+                logo: "img/eudc_bank_logo.png"
+            };
+
+        function updateStyles(bank, bankId) {
+            $('a[role=main-action]').each(function () {
+                $(this).attr('href', ($(this).attr('href') + '?bank=' + bankId));
+            });
+            $("img.logo").attr("src", bank.logo);
+            $('title').html(bank.title);
+            $('footer span.bank-name').html(bank.footer);
+            $('link#bootstrap').prop('disabled', false);
+            $("link#theme").attr("href", bank.style);
+        }
+
+        if (location.search) {
+            var style = location.search.replace("?", "").split("=");
+            (style[0] === "bank" && parseInt(style[1]) === 2) ? updateStyles(eudcBank, 2) : updateStyles(citizenBank, 1);
+        }
+        else {
+            updateStyles(citizenBank, 1);
+        }
+    }
 });
